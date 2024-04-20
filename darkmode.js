d3a547090525bec912a9b0852630c0348e38d5f2
MichaelENewbold <122341345+MichaelENewbold@users.noreply.github.com>
2024-04-20 14:44:07 -0700

    Initial Push

---
+++ darkmode.js
@@ -0,0 +1,7 @@
+// darkmode.js
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    // Toggle the 'dark-mode' class on the body element
+    document.body.classList.toggle('dark-mode');
+}
